1cd3eeaa5a1c0727e283a5e5f4006428f50ce0b5
Arthur-Candeia <arthurcandeiahc@gmail.com>
2023-02-09 00:24:14 -0300

    imagens-clicar

--- js/script.js
+++ js/script.js
@@ -17,33 +17,33 @@ function clickMenu() {
 }
 
 function picture1() {
-  exibir.style.backgroundImage = 'url(../images/foto1.jpg)';
-  img01.style.transform = 'scale(1.1)';
-  img02.style.transform = 'scale(1)';
-  img03.style.transform = 'scale(1)';
-  img04.style.transform = 'scale(1)';
+  exibir.style.backgroundImage = 'url(images/foto1.jpg)'
+  img01.style.transform = 'scale(1.1)'
+  img02.style.transform = 'scale(1)'
+  img03.style.transform = 'scale(1)'
+  img04.style.transform = 'scale(1)'
 }
 
 function picture2() {
-  exibir.style.backgroundImage = 'url(../images/foto2.jpg)';
-  img01.style.transform = 'scale(1)';
-  img02.style.transform = 'scale(1.1)';
-  img03.style.transform = 'scale(1)';
-  img04.style.transform = 'scale(1)';
+  exibir.style.backgroundImage = 'url(images/foto2.jpg)'
+  img01.style.transform = 'scale(1)'
+  img02.style.transform = 'scale(1.1)'
+  img03.style.transform = 'scale(1)'
+  img04.style.transform = 'scale(1)'
 }
 
 function picture3() {
-  exibir.style.backgroundImage = 'url(../images/foto3.jpg)'
-  img01.style.transform = 'scale(1)';
-  img02.style.transform = 'scale(1)';
-  img03.style.transform = 'scale(1.1)';
-  img04.style.transform = 'scale(1)';
+  exibir.style.backgroundImage = 'url(images/foto3.jpg)'
+  img01.style.transform = 'scale(1)'
+  img02.style.transform = 'scale(1)'
+  img03.style.transform = 'scale(1.1)'
+  img04.style.transform = 'scale(1)'
 }
 
 function picture4() {
-  exibir.style.backgroundImage = 'url(../images/foto4.jpg)'
-  img01.style.transform = 'scale(1)';
-  img02.style.transform = 'scale(1)';
-  img03.style.transform = 'scale(1)';
-  img04.style.transform = 'scale(1.1)';
+  exibir.style.backgroundImage = 'url(images/foto4.jpg)'
+  img01.style.transform = 'scale(1)'
+  img02.style.transform = 'scale(1)'
+  img03.style.transform = 'scale(1)'
+  img04.style.transform = 'scale(1.1)'
 }
