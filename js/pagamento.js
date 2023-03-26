758c4fbec21fcb73cc6e34ee58c8ff378bd8d1aa
Arthur-Candeia <arthurheher@hotmail.com>
2023-03-26 17:52:24 -0300

    verificacao-pagamento

--- js/pagamento.js
+++ js/pagamento.js
@@ -1,6 +1,7 @@
 function numcard() {
   let numeroDigitado = numero.value
   numeroCartao.innerHTML = numeroDigitado
+
   if (numeroDigitado[0] == '4') { //Bandeira Visa
     document.getElementById('visa').style.visibility = 'visible'
     document.getElementById('mastercard').style.visibility = 'hidden'
@@ -22,6 +23,7 @@ function numcard() {
     document.getElementById('bandeiraDesconhecida').style.display = 'block'
     document.getElementById('bandeiraDesconhecida').innerHTML = 'Band. desc.'
   }
+
   if (numeroDigitado.length == 4 || numeroDigitado.length == 9 || numeroDigitado.length == 14) {
     numero.value += ' '
   }
@@ -43,9 +45,30 @@ function namecard() {
 function datecard() {
   let dataDigitada = data.value
   dataCartao.innerHTML = dataDigitada
+  
 }
 
 function colorcard() {
   let corEscolhida = cor.value
   cartao.style.backgroundColor = corEscolhida
 }
+
+function comprar() { /*Verificação*/
+  if (numero.value[18] !== undefined && nome.value[9] !== undefined && data.value !== '') {
+    window.location.href = "../pags/pagamento-confirmado.html"
+  }
+}
+
+/*OUTRA FORMA DE FAZER A VERIFICAÇÃO, SÓ QUE O FORM TERIA QUE SER PREENCHIDO OBRIGATORIAMENTE NA ORDEM
+let number = document.getElementById('numero')
+
+let TESTEdata = document.getElementById('data')
+
+TESTEdata.addEventListener('change', (event) => {
+  pagina = document.querySelector('#mandar');
+  if(event.target) {
+    if (numero.value[18] !== undefined && nome.value[9] !== undefined) {
+      window.location.href = "http://www.devmedia.com.br"
+    }
+  }
+})*/
